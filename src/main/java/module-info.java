04ebb9cc00507235afa5840882017dda3908f19b
Tinashe Matanda <tinashelorenzi@protonmail.com>
2025-07-07 21:44:24 +0200

    Execution fixed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.drlab.drlabdesktop {
 
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires atlantafx.base;
 
     opens com.drlab.drlabdesktop to javafx.fxml;
     exports com.drlab.drlabdesktop;
